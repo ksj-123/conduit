135ccd338d8912f1998df7f97bdaefd37f189a71
Kissne Simko Judit <kissnesimkojudit@gmail.com>
2021-07-25 21:49:00 +0200

    up

--- testcases/TC005.py
+++ testcases/TC005.py
@@ -1,48 +1,33 @@
 # New blog post
-import csv
-
 from selenium import webdriver
 # from webdriver_manager.chrome import ChromeDriverManager
 import time
+import csv
 
 driver = webdriver.Chrome()
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 
 try:
-    driver.get("https://react-layr-realworld-example-app.layrjs.com/all")
-    time.sleep(3)
-
-
-    def signin(email, password):
+    def sign_in(email, password):
+        driver.get("https://react-layr-realworld-example-app.layrjs.com/all")
         driver.find_element_by_link_text('Sign in').click()
-        with open('registration.csv') as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=',')
-            next(csvreader)
-            for row in csvreader:
-                print(row)
-                email = driver.find_element_by_tag_name('Email').send_keys(row[1])
-                password = driver.find_element_by_tag_name('Password').send_keys(row[2])
-                signin(email, password)
-            driver.find_element_by_class_name('btn').click()
-        signin(email, password)
-    time.sleep(2)
+        email = driver.find_element_by_tag_name('Email').send_keys('username1@gmail.com')
+        password = driver.find_element_by_tag_name('Password').send_keys('Username1')
+        driver.find_element_by_class_name('btn').click()
+    sign_in('user1@gmail.com', 'User1')
 
-
-    def new_post(title, about, write, tags):
-        driver.find_element_by_xpath('//*[@id="root"]/div/nav/div/ul/li[2]/a').click()
-        with open('post.csv') as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=',')
-            next(csvreader)
-            for row in csvreader:
-                print(row)
-                title = driver.find_element_by_tag_name('Article title').send_keys(row[0])
-                about = driver.find_element_by_tag_name("What's this article about?").send_keys(row[1])
-                write = driver.find_element_by_link_text('Write your article (in markdown)').send_keys(row[2])
-                tags = driver.find_element_by_name('Enter tags').send_keys(row[3])
-                new_post(title, about, write, tags)
-            driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div/div/form/fieldset/button').click()
-        new_post(title, about, write, tags)
-    time.sleep(2)
+    # driver.find_element_by_xpath('//*[@id="root"]/div/nav/div/ul/li[2]/a').click()
+    # with open('post.csv') as csvfile:
+    #     csvreader = csv.reader(csvfile, delimiter=',')
+    #     next(csvreader)
+    #     for row in csvreader:
+    #         print(row)
+    #         title = driver.find_element_by_tag_name('Article title').send_keys(row[0])
+    #         about = driver.find_element_by_tag_name("What's this article about?").send_keys(row[1])
+    #         write = driver.find_element_by_link_text('Write your article (in markdown)').send_keys(row[2])
+    #         tags = driver.find_element_by_name('Enter tags').send_keys(row[3])
+    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div/div/form/fieldset/button').click()
+    # time.sleep(2)
 
 finally:
     pass
